2267e93c47be9eb615b3fa3deac4d5ea7cd64404
Matthias Wiemann <mat@wiemann.name>
2021-02-10 19:51:14 +0100

    remove sample code, add headline and add e2e test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="cv">
+    <h1>Curriculum Vitae</h1>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld
-  }
-})
-export default class Home extends Vue {}
+@Component
+export default class CV extends Vue {}
 </script>
