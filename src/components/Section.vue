862cfbf46eb8a7e8a2dd7d81870a8c1e0d7434fc
Matthias Wiemann <mat@wiemann.name>
2021-02-11 13:09:45 +0100

    add my personal / contact data, profile picture and typescript definitions for md and yaml packages

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="section">
-    <h3>{{ title }}</h3>
-    <p class="time-range">{{ timeRange }}</p>
+    <h3 v-if="title">{{ title }}</h3>
+    <i v-if="timeRange" class="time-range">{{ timeRange }}</i>
     <div class="main">
       <vue-markdown>
         <slot></slot>
@@ -18,7 +18,15 @@ import VueMarkdown from "vue-markdown";
   components: { VueMarkdown }
 })
 export default class Section extends Vue {
-  @Prop() private readonly title!: string;
-  @Prop() private readonly timeRange!: string;
+  @Prop() private readonly title: string | null;
+  @Prop() private readonly timeRange: string | null;
 }
 </script>
+
+<style scoped lang="scss">
+.section {
+  h3 {
+    margin-top: 3rem;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,33 @@
 <template>
   <div class="cv">
     <h1>Curriculum Vitae</h1>
-    <div v-for="(category, index) in data.categories" :key="index">
+    <img
+      class="profile-picture"
+      :src="data.picture.src"
+      :width="data.picture.width / 2"
+      :height="data.picture.height / 2"
+    />
+    <div class="h-card vcard">
+      <p class="p-name fn">{{ data.name }}</p>
+      <p class="title">{{ data.title }}</p>
+      <p class="email">
+        Mail: <a :href="`mailto:${data.email}`"> {{ data.email }}</a>
+      </p>
+      <p class="p-tel tel">
+        Phone: <a :href="`tel:${data.phone}`">{{ data.phone }}</a>
+      </p>
+      <p class="adr">Location: {{ data.address }}</p>
+      <p class="timezone">
+        Timezone: {{ data.timeZone
+        }}<span class="tz">{{ data.timeZoneOffset }}</span>
+      </p>
+      <p>Year of Birth: {{ data.birthYear }}</p>
+    </div>
+    <div
+      v-for="(category, index) in data.categories"
+      :key="index"
+      class="chapter"
+    >
       <h2>{{ category.title }}</h2>
       <Section
         v-for="(item, index) in category.items"
@@ -12,6 +38,7 @@
         {{ item.description }}
       </Section>
     </div>
+    <p class="last-update">Last update: {{ data.lastUpdate }}</p>
   </div>
 </template>
 
@@ -40,3 +67,23 @@ export default class CV extends Vue {
   }
 }
 </script>
+
+<style scoped lang="scss">
+.cv {
+  h2 {
+    margin-top: 3rem;
+  }
+  .last-update {
+    padding-top: 0.5rem;
+    margin-top: 2rem;
+    border-top: 1px solid black;
+    text-align: right;
+  }
+
+  @media only screen and (min-width: 600px) {
+    .profile-picture {
+      float: right;
+    }
+  }
+}
+</style>
